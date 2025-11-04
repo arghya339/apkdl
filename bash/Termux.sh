c78864628804eaf27439c4745e36724c5c84df91
Arghyadeep <139842491+arghya339@users.noreply.github.com>
2025-11-04 22:46:45 +0530

    fix

---
+++ bash/Termux.sh
@@ -0,0 +1,198 @@
+#!/usr/bin/bash
+
+  pkg update > /dev/null 2>&1 || apt update >/dev/null 2>&1  # It downloads latest package list with versions from Termux remote repository, then compares them to local (installed) pkg versions, and shows a list of what can be upgraded if they are different.
+  outdatedPKG=$(apt list --upgradable 2>/dev/null)  # list of outdated pkg
+  echo "$outdatedPKG" | grep -q "dpkg was interrupted" 2>/dev/null && { yes "N" | dpkg --configure -a; outdatedPKG=$(apt list --upgradable 2>/dev/null); }
+  installedPKG=$(pkg list-installed 2>/dev/null)  # list of installed pkg
+
+  # --- Storage Permission Check Logic ---
+  if ! ls /sdcard/ 2>/dev/null | grep -E -q "^(Android|Download)"; then
+    echo -e "${notice} ${Yellow}Storage permission not granted!${Reset}\n$running ${Green}termux-setup-storage${Reset}.."
+    if [ "$Android" -gt 5 ]; then  # for Android 5 storage permissions grant during app installation time, so Termux API termux-setup-storage command not required
+      count=0
+      while true; do
+        if [ "$count" -ge 2 ]; then
+          echo -e "$bad Failed to get storage permissions after $count attempts!"
+          echo -e "$notice Please grant permissions manually in Termux App info > Permissions > Files > File permission → Allow."
+          am start -a android.settings.APPLICATION_DETAILS_SETTINGS -d package:com.termux &> /dev/null
+          exit 0
+        fi
+        termux-setup-storage  # ask Termux Storage permissions
+        sleep 3  # wait 3 seconds
+        if ls /sdcard/ 2>/dev/null | grep -q "^Android" || ls "$HOME/storage/shared/" 2>/dev/null | grep -q "^Android"; then
+          if [ "$Android" -lt 8 ]; then
+            exit 0  # Exit the script
+          fi
+          break
+        fi
+        ((count++))
+      done
+    fi
+  fi
+
+  # --- enabled allow-external-apps ---
+  isOverwriteTermuxProp=0
+  if [ $Android -eq 6 ] && [ ! -f "$HOME/.termux/termux.properties" ]; then
+    mkdir -p "$HOME/.termux" && echo "allow-external-apps = true" > "$HOME/.termux/termux.properties"
+    isOverwriteTermuxProp=1
+    echo -e "$notice 'termux.properties' file has been created successfully & 'allow-external-apps = true' line has been add (enabled) in Termux \$HOME/.termux/termux.properties."
+    termux-reload-settings
+  elif [ $Android -eq 6 ] && [ -f "$HOME/.termux/termux.properties" ]; then
+    if grep -q "^# allow-external-apps" "$HOME/.termux/termux.properties"; then
+      sed -i '/allow-external-apps/s/# //' "$HOME/.termux/termux.properties"  # uncomment 'allow-external-apps = true' line
+      isOverwriteTermuxProp=1
+      echo -e "$notice 'allow-external-apps = true' line has been uncommented (enabled) in Termux \$HOME/.termux/termux.properties."
+      termux-reload-settings
+    fi
+  fi
+  if [ "$Android" -ge 6 ]; then
+    if grep -q "^# allow-external-apps" "$HOME/.termux/termux.properties"; then
+      # other Android applications can send commands into Termux.
+      # termux-open utility can send an Android Intent from Termux to Android system to open apk package file in pm.
+      # other Android applications also can be Access Termux app data (files).
+      sed -i '/allow-external-apps/s/# //' "$HOME/.termux/termux.properties"  # uncomment 'allow-external-apps = true' line
+      isOverwriteTermuxProp=1
+      echo -e "$notice 'allow-external-apps = true' line has been uncommented (enabled) in Termux \$HOME/.termux/termux.properties."
+      #if [ "$Android" -eq 7 ] || [ "$Android" -eq 6 ]; then
+        termux-reload-settings  # reload (restart) Termux settings required for Android 6 after enabled allow-external-apps, also required for Android 7 due to 'Package installer has stopped' err
+      #fi
+    fi
+  fi
+
+  su -c "id" >/dev/null 2>&1 && su=1 || su=0
+
+  # --- Shizuku Setup first time ---
+  if [ $su -eq 0 ] && { [ ! -f "$HOME/rish" ] || [ ! -f "$HOME/rish_shizuku.dex" ]; }; then
+    #echo -e "$info Please manually install Shizuku from Google Play Store." && sleep 1
+    #termux-open-url "https://play.google.com/store/apps/details?id=moe.shizuku.privileged.api"
+    echo -e "$info Please manually install Shizuku from GitHub." && sleep 1
+    termux-open-url "https://github.com/RikkaApps/Shizuku/releases/latest"
+    am start -n com.android.settings/.Settings\$MyDeviceInfoActivity > /dev/null 2>&1  # Open Device Info
+
+    curl -sL -o "$HOME/rish" "https://raw.githubusercontent.com/arghya339/crdl/refs/heads/main/Termux/Shizuku/rish" && chmod +x "$HOME/rish"
+    sleep 0.5 && curl -sL -o "$HOME/rish_shizuku.dex" "https://raw.githubusercontent.com/arghya339/crdl/refs/heads/main/Termux/Shizuku/rish_shizuku.dex"
+  
+    if [ "$Android" -lt 11 ]; then
+      url="https://youtu.be/ZxjelegpTLA"  # YouTube/@MrPalash360: Start Shizuku using Computer
+      activityClass="com.android.settings/.Settings\$DevelopmentSettingsDashboardActivity"  # Open Developer options
+    else
+      activityClass="com.android.settings/.Settings\$WirelessDebuggingActivity"  # Open Wireless Debugging Settings
+      url="https://youtu.be/YRd0FBfdntQ"  # YouTube/@MrPalash360: Start Shizuku Android 11+
+    fi
+    echo -e "$info Please start Shizuku by following guide: $url" && sleep 1
+    am start -n "$activityClass" > /dev/null 2>&1
+    termux-open-url "$url"
+  fi
+  if ! "$HOME/rish" -c "id" >/dev/null 2>&1 && [ -f "$HOME/rish_shizuku.dex" ]; then
+    if ~/rish -c "id" 2>&1 | grep -q 'java.lang.UnsatisfiedLinkError'; then
+      rm -f "$HOME/rish_shizuku.dex" && curl -sL -o "$HOME/rish_shizuku.dex" "https://raw.githubusercontent.com/arghya339/crdl/refs/heads/main/Termux/Shizuku/Play/rish_shizuku.dex"
+    fi
+  fi
+
+  if [ "$(getprop ro.product.manufacturer)" == "Genymobile" ] && [ ! -f "$HOME/adb" ]; then
+    curl -sL -o "$HOME/adb" "https://raw.githubusercontent.com/rendiix/termux-adb-fastboot/refs/heads/master/binary/${cpuAbi}/bin/adb" && chmod +x ~/adb
+  fi
+
+  # --- pkg uninstall function ---
+  pkgUninstall() {
+    local pkg=$1
+    if echo "$installedPKG" | grep -q "^$pkg/" 2>/dev/null; then
+      echo -e "$running Uninstalling $pkg pkg.."
+      pkg uninstall "$pkg" -y > /dev/null 2>&1
+    fi
+  }
+
+  # --- pkg upgrade function ---
+  pkgUpdate() {
+  local pkg=$1
+    if echo "$outdatedPKG" | grep -q "^$pkg/" 2>/dev/null; then
+      echo -e "$running Upgrading $pkg pkg.."
+      output=$(pkg install --only-upgrade "$pkg" -y 2>/dev/null)
+      echo "$output" | grep -q "dpkg was interrupted" 2>/dev/null && { yes "N" | dpkg --configure -a; yes "N" | pkg install --only-upgrade "$pkg" -y > /dev/null 2>&1; }
+    fi
+  }
+
+  # --- pkg install/update function ---
+  pkgInstall() {
+    local pkg=$1
+    if echo "$installedPKG" | grep -q "^$pkg/" 2>/dev/null; then
+      pkgUpdate "$pkg"
+    else
+      echo -e "$running Installing $pkg pkg.."
+      pkg install "$pkg" -y > /dev/null 2>&1
+    fi
+  }
+
+  #pkgInstall "apt"  # apt update
+  pkgInstall "dpkg"  # dpkg update
+  #pkgInstall "bash"  # bash update
+  pkgInstall "libgnutls"  # pm apt & dpkg use it to securely download packages from repositories over HTTPS
+  #pkgInstall "coreutils"  # It provides basic file, shell, & text manipulation utilities. such as: ls, cp, mv, rm, mkdir, cat, echo, etc.
+  pkgInstall "termux-core"  # it's contains basic essential cli utilities, such as: ls, cp, mv, rm, mkdir, cat, echo, etc.
+  pkgInstall "termux-tools"  # it's provide essential commands, sush as: termux-change-repo, termux-setup-storage, termux-open, termux-share, etc.
+  pkgInstall "termux-keyring"  # it's use during pkg install/update to verify digital signature of the pkg and remote repository
+  pkgInstall "termux-am"  # termux am (activity manager) update
+  pkgInstall "termux-am-socket"  # termux am socket (when run: am start -n activity ,termux-am take & send to termux-am-stcket and it's send to Termux Core to execute am command) update
+  pkgInstall "inetutils"  # ping utils is provided by inetutils
+  pkgInstall "util-linux"  # it provides: kill, killall, uptime, uname, chsh, lscpu
+  pkgInstall "libsmartcols"  # a library from the util-linux pkg
+  pkgInstall "curl"  # curl update
+  pkgInstall "libcurl"  # curl lib update
+  pkgInstall "aria2"  # aria2 install/update
+  #pkgInstall "openssl"  # openssl install/update
+  pkgInstall "jq"  # jq install/update
+  pkgInstall "pup"  # pup install/update
+  pkgInstall "openjdk-21"  # java install/update
+  pkgInstall "bsdtar"  # bsdtar install/update
+  pkgInstall "pv"  # pv install/update
+  pkgInstall "grep"  # grep update
+  pkgInstall "gawk"  # gnu awk update
+  pkgInstall "sed"  # sed update
+  pkgInstall "findutils"  # find utils update
+  pkgInstall "glow"  # glow install/update
+
+  # Create apkdl config
+  all_key=("RipLocale" "RipDpi" "RipLib")
+  all_value=("$isRipLocale" "$isRipDpi" "$isRipLib")
+  # Loop through all keys and set values if they don't exist
+  for i in "${!all_key[@]}"; do
+    ! jq -e --arg key "${all_key[i]}" 'has($key)' "$apkdlJson" >/dev/null && config "${all_key[i]}" "${all_value[i]}"
+  done
+
+  # Get RipLocale value from json
+  jq -e '.RipLocale != null' "$apkdlJson" >/dev/null 2>&1 && RipLocale="$(jq -r '.RipLocale' "$apkdlJson" 2>/dev/null)" || RipLocale=1
+  # Get RipDpi value from json
+  jq -e '.RipDpi != null' "$apkdlJson" >/dev/null 2>&1 && RipDpi="$(jq -r '.RipDpi' "$apkdlJson" 2>/dev/null)" || RipDpi=1
+  # Get RipLib value from json
+  jq -e '.RipLib != null' "$apkdlJson" >/dev/null 2>&1 && RipLib="$(jq -r '.RipLib' "$apkdlJson" 2>/dev/null)" || RipLib=1
+
+  # Build locale
+  if [ $RipLocale -eq 1 ]; then
+    locale=$(getprop persist.sys.locale | cut -d'-' -f1)  # Get System Languages
+    [ -z $locale ] && locale=$(getprop ro.product.locale | cut -d'-' -f1)  # Get Languages
+  elif [ $RipLocale -eq 0 ]; then
+    locale="[a-z][a-z]"
+  fi
+
+  # Build lcd_dpi
+  if [ $RipDpi -eq 1 ]; then
+    density=$(getprop ro.sf.lcd_density)  # Get the device screen density
+    # Check and categorize the density
+    if [ "$density" -le 120 ]; then
+      lcd_dpi="ldpi"  # Low Density
+    elif [ "$density" -le 160 ]; then
+      lcd_dpi="mdpi"  # Medium Density
+    elif [ "$density" -le 240 ]; then
+      lcd_dpi="hdpi"  # High Density
+    elif [ "$density" -le 320 ]; then
+      lcd_dpi="xhdpi"  # Extra High Density
+    elif [ "$density" -le 480 ]; then
+      lcd_dpi="xxhdpi"  # Extra Extra High Density
+    elif [ "$density" -gt 480 ] || [ "$density" -ge 640 ]; then
+      lcd_dpi="xxxhdpi"  # Extra Extra Extra High Density
+    else
+      lcd_dpi="*dpi"
+    fi
+  elif [ $RipDpi -eq 0 ]; then
+    lcd_dpi="*dpi"
+  fi
---
+++ bash/macOS.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+  # --- Check if brew is installed ---
+  if brew --version >/dev/null 2>&1; then
+    brew update > /dev/null 2>&1
+  else
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null 2>&1
+  fi
+  formulaeList=$(brew list 2>/dev/null)
+  outdatedFormulae=$(brew outdated 2>/dev/null)
+
+  # --- formulae upgrade function ---
+  formulaeUpdate() {
+    local formulae=$1
+    if echo "$outdatedFormulae" | grep -q "^$formulae" 2>/dev/null; then
+      echo -e "$running Upgrading $formulae formulae.."
+      brew upgrade "$formulae" > /dev/null 2>&1
+    fi
+  }
+
+  # --- formulae install/update function ---
+  formulaeInstall() {
+    local formulae=$1
+    if echo "$formulaeList" | grep -q "$formulae" 2>/dev/null; then
+      formulaeUpdate "$formulae"
+    else
+      echo -e "$running Installing $formulae formulae.."
+      brew install "$formulae" > /dev/null 2>&1
+    fi
+  }
+
+  formulaeInstall "bash"  # bash update
+  formulaeInstall "grep"  # grep update
+  formulaeInstall "curl"  # curl update
+  formulaeInstall "aria2"  # aria2 install/update
+  formulaeInstall "ca-certificate"  # ca-certificate update
+  formulaeInstall "jq"  # jq install/update
+  formulaeInstall "pv"  # pv install/update
+  formulaeInstall "pup"  # pup install/update
+  formulaeInstall "android-platform-tools"  # android-platform-tools install/update
+  formulaeInstall "openjdk"  # java install/update
+  grep -q 'export PATH="/usr/local/opt/openjdk/bin:$PATH"' ~/.zshrc 2>/dev/null || echo 'export PATH="/usr/local/opt/openjdk/bin:$PATH"' >> ~/.zshrc
+  # https://github.com/aria2/aria2/issues/1920
+  aria2Executing=$(aria2c -q -d "$HOME" -o aria2Executing -U "User-Agent: $USER_AGENT" -U "Referer: https://one.one.one.one/" --ca-certificate="/etc/ssl/cert.pem" --async-dns=true --async-dns-server="$cloudflareIP" "https://one.one.one.one/")
+  if echo "$aria2Executing" | grep -q "--async-dns=true" 2>/dev/null; then
+    curl -L --progress-bar -C - -o $HOME/Downloads/aria2c-macos-$Arch.tar https://github.com/tofuliang/aria2/releases/download/20240919/aria2c-macos-$Arch.tar
+    pv "$HOME/Downloads/aria2c-macos-$Arch.tar" | tar -xf - -C "$HOME/Downloads" && rm -f "$HOME/Downloads/aria2c-macos-$Arch.tar"
+    sudo mv $HOME/Downloads/aria2c /usr/local/bin/aria2c
+    if aria2c -v &>/dev/null; then
+      aria2c -v | head -1 | awk '{print $3}'
+    else
+      sudo xattr -d com.apple.quarantine /usr/local/bin/aria2c && aria2c -v | head -1 | awk '{print $3}'
+    fi
+    rm -f ~/aria2Executing
+  else
+    rm -f ~/aria2Executing
+  fi
+  getSerial() {
+    deviceCount=$(adb devices | grep -c "device$")
+    if [ $deviceCount -eq 0 ]; then
+      serial=
+    elif [ $deviceCount -eq 1 ]; then
+      serial=$(adb devices | grep "device$" | awk '{print $1}')
+    elif [ $deviceCount -gt 1 ]; then
+      serials=($(adb devices | grep "device$" | awk '{print $1}'))
+      devices=()
+      for i in "${!serials[@]}"; do
+        serial="${serials[i]}"
+        model=$(adb -s $serial shell getprop ro.product.model)
+        devices+=("$model ($serial)")
+      done
+      buttons=("<Select>" "<Back>")
+      if menu "devices" "buttons"; then
+        serial="${serials[$selected]}"
+      fi
+    fi
+    [ $deviceCount -gt 0 ] && echo -e "$info serial: $serial"
+  }; getSerial
