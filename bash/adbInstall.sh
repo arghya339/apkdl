9617c341a308091330da0b3fc94753d077294c75
Arghyadeep <139842491+arghya339@users.noreply.github.com>
2025-11-05 20:14:43 +0530

    fix

--- bash/adbInstall.sh
+++ bash/adbInstall.sh
@@ -4,7 +4,7 @@ aapt2=("$HOME/Library/Android/sdk/build-tools/"*/aapt2) && aapt2="${aapt2[-1]}"
 
 adbInstall() {
   local outputAPK="${1}"
-  local outputFileName=$(basename "$outputAPK")
+  local outputFileName="$(basename "$outputAPK")"
   app_info=$($aapt2 dump badging "$outputAPK" 2>/dev/null)
   pkgName=$(awk -F"'" '/package/ {print $2}' <<< "$app_info" | head -1)
   appName=$(awk -F"'" '/application-label:/ {print $2}' <<< "$app_info")
