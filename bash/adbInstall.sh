a1a08092d6f432a5573ad2f400d14ffa95c793f7
Arghyadeep <139842491+arghya339@users.noreply.github.com>
2025-11-05 19:34:02 +0530

    fix

--- bash/adbInstall.sh
+++ bash/adbInstall.sh
@@ -11,7 +11,7 @@ adbInstall() {
   appName=$(awk -F"'" '/application-label:/ {print $2}' <<< "$app_info")
   local activityClass=$(adb -s $serial shell "pm resolve-activity --brief $pkgName" | tail -n 1)
   Android=$(adb -s $serial shell getprop ro.build.version.release | cut -d. -f1)
-  adb -s $serial shell cp "$outputAPK" "/data/local/tmp/$outputFileName"
+  adb -s $serial push "$outputAPK" "/data/local/tmp/$outputFileName"
   [ $DisablePlayProtect -eq 1 ] && adb -s $serial shell "settings put global package_verifier_user_consent -1"  # Disabled Play Protect
   if [ $DisableVerifyAdbInstalls -eq 1 ]; then
     [ $Android -le 10 ] && adb -s $serial shell "settings put global package_verifier_enable 0" || adb -s $serial shell "settings put global verifier_verify_adb_installs 0"  # Disable Verify Adb Installs
