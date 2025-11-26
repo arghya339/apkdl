c78ca636f50f46a90f39a3a99c762d1ddba8f6a7
Arghyadeep <139842491+arghya339@users.noreply.github.com>
2025-11-26 10:30:51 +0530

    fix

--- bash/apkInstall.sh
+++ bash/apkInstall.sh
@@ -8,145 +8,73 @@ Android=$(getprop ro.build.version.release | cut -d. -f1)
 apkInstall() {
   local outputAPK=${1}
   local activity=$2  # for non-rooted user to launch app after installtion
+  iCmd() {
+    cmd=${1}
+    if [ $su -eq 1 ]; then
+      su -c "$cmd"
+    elif "$HOME/rish" -c "id" >/dev/null 2>&1; then
+      ~/rish -c "$cmd"
+    elif "$HOME/adb" -s $(~/adb devices | grep "device$" | awk '{print $1}' | tail -1) shell "id" >/dev/null 2>&1; then
+      ~/adb -s $("$HOME/adb" devices 2>/dev/null | grep "device$" | awk '{print $1}' | tail -1) shell "$cmd"
+    fi
+  }
+  
   local outputFileName=$(basename "$outputAPK")
   app_info=$($HOME/aapt2 dump badging "$outputAPK" 2>/dev/null)
   pkgName=$(awk -F"'" '/package/ {print $2}' <<< "$app_info" | head -1)
   appName=$(awk -F"'" '/application-label:/ {print $2}' <<< "$app_info")
   if [ $su -eq 1 ]; then
-    if [ "$(su -c 'getenforce 2>/dev/null')" = "Enforcing" ]; then
-      su -c "setenforce 0"
-      local activityClass=$(su -c "pm resolve-activity --brief $pkgName" | tail -n 1)
-      su -c "setenforce 1"
-    else
-      local activityClass=$(su -c "pm resolve-activity --brief $pkgName" | tail -n 1)
-    fi
-  elif "$HOME/rish" -c "id" >/dev/null 2>&1; then
-    local activityClass=$($HOME/rish -c "pm resolve-activity --brief $pkgName" | tail -n 1)
-  elif "$HOME/adb" -s $(~/adb devices | grep "device$" | awk '{print $1}' | tail -1) shell "id" >/dev/null 2>&1; then
-    local activityClass=$($HOME/adb -s $(~/adb devices | grep "device$" | awk '{print $1}' | tail -1) shell "pm resolve-activity --brief $pkgName" | tail -n 1)
+    [ "$(su -c 'getenforce 2>/dev/null')" = "Enforcing" ] && { su -c "setenforce 0"; writeSELinux=1; } || writeSELinux=0
+  fi
+  if [ $su -eq 1 ] || "$HOME/rish" -c "id" >/dev/null 2>&1 || "$HOME/adb" -s $(~/adb devices | grep "device$" | awk '{print $1}' | tail -1) shell "id" >/dev/null 2>&1; then
+    iCmdOut=$(iCmd "pm resolve-activity --brief $pkgName")
+    local activityClass=$(tail -n 1 <<< "$iCmdOut") && unset iCmdOut
   else
     local activityClass="$activity"
   fi
-  
-  if [ $su -eq 1 ]; then
-    su -c "cp '$outputAPK' '/data/local/tmp/$outputFileName'"
-    # Temporary Disable SELinux Enforcing during installation if it not in Permissive
-    if [ "$(su -c 'getenforce 2>/dev/null')" = "Enforcing" ]; then
-      su -c "setenforce 0"  # set SELinux to Permissive mode to unblock unauthorized operations
-      [ $DisablePlayProtect -eq 1 ] && su -c "settings put global package_verifier_user_consent -1"  # Disabled Play Protect
-      if [ $DisableVerifyAdbInstalls -eq 1 ]; then
-        [ $Android -le 10 ] && su -c "settings put global package_verifier_enable 0" || su -c "settings put global verifier_verify_adb_installs 0"  # Disable Verify Adb Installs
-      fi
-      output=$(su -c "pm install ${cmd} \"/data/local/tmp/${outputFileName}\"" 2>&1); echo "$output"
-      [ $DisablePlayProtect -eq 1 ] && su -c "settings put global package_verifier_user_consent 1"  # Enabled Play Protect
-      if [ $DisableVerifyAdbInstalls -eq 1 ]; then
-        [ $Android -le 10 ] && su -c "settings put global package_verifier_enable 1" || su -c "settings put global verifier_verify_adb_installs 1"  # Enabled Verify Adb Installs
-      fi
-      if [[ $output == *"Downgrade detected"* ]] && [ $KeepsData -eq 1 ]; then
-        echo -e "${Green}$appName uninstall successfully with keeps app data.${Reset}\n${Yellow}Don't forget to restart Simplify after reboot!${Reset}"
-        su -c "cmd package uninstall -k $pkgName"
-        cp "$outputAPK" "$POST_INSTALL"
-        sleep 12
-        su -c "reboot"
-      fi
-      su -c "setenforce 1"  # set SELinux to Enforcing mode to block unauthorized operations
-    else
-      [ $DisablePlayProtect -eq 1 ] && su -c "settings put global package_verifier_user_consent -1"  # Disabled Play Protect
-      if [ $DisableVerifyAdbInstalls -eq 1 ]; then
-        [ $Android -le 10 ] && su -c "settings put global package_verifier_enable 0" || su -c "settings put global verifier_verify_adb_installs 0"  # Disable Verify Adb Installs
-      fi
-      output=$(su -c "pm install ${cmd} \"/data/local/tmp/${outputFileName}\"" 2>&1); echo "$output"
-      [ $DisablePlayProtect -eq 1 ] && su -c "settings put global package_verifier_user_consent 1"  # Enabled Play Protect
-      if [ $DisableVerifyAdbInstalls -eq 1 ]; then
-        [ $Android -le 10 ] && su -c "settings put global package_verifier_enable 1" || su -c "settings put global verifier_verify_adb_installs 1"  # Enabled Verify Adb Installs
-      fi
-      if [[ $output == *"Downgrade detected"* ]] && [ $KeepsData -eq 1 ]; then
-        echo -e "${Green}$appName uninstall successfully with keeps app data.${Reset}\n${Yellow}Don't forget to restart Simplify after reboot!${Reset}"
-        su -c "cmd package uninstall -k $pkgName"
-        cp "$outputAPK" "$POST_INSTALL"
-        sleep 12
-        su -c "reboot"
-      fi
-    fi
-    am start -n "$activityClass" &> /dev/null  # launch app after update
-    if [ $? != 0 ]; then
-      su -c "monkey -p $pkgName -c android.intent.category.LAUNCHER 1" > /dev/null 2>&1
-    fi
-    su -c "rm -f '/data/local/tmp/$outputFileName'"
-  elif "$HOME/rish" -c "id" >/dev/null 2>&1; then
-    ~/rish -c "cp '$outputAPK' '/data/local/tmp/$outputFileName'" > /dev/null 2>&1  # copy apk to System dir
-    [ $DisablePlayProtect -eq 1 ] && $HOME/rish -c "settings put global package_verifier_user_consent -1"  # Disabled Play Protect
+  if [ $su -eq 1 ] || "$HOME/rish" -c "id" >/dev/null 2>&1 || "$HOME/adb" -s $(~/adb devices | grep "device$" | awk '{print $1}' | tail -1) shell "id" >/dev/null 2>&1; then
+    iCmd "cp '$outputAPK' '/data/local/tmp/$outputFileName'"
+    [ $DisablePlayProtect -eq 1 ] && iCmd "settings put global package_verifier_user_consent -1"  # Disabled Play Protect
     if [ $DisableVerifyAdbInstalls -eq 1 ]; then
-      [ $Android -le 10 ] && $HOME/rish -c "settings put global package_verifier_enable 0" || $HOME/rish -c "settings put global verifier_verify_adb_installs 0"  # Disable Verify Adb Installs
+      [ $Android -le 10 ] && iCmd "settings put global package_verifier_enable 0" || iCmd "settings put global verifier_verify_adb_installs 0"  # Disable Verify Adb Installs
     fi
-    output=$(~/rish -c "pm install ${cmd} \"/data/local/tmp/${outputFileName}\"" 2>&1); echo "$output"
-    [ $DisablePlayProtect -eq 1 ] && ~/rish -c "settings put global package_verifier_user_consent 1"  # Enabled Play Protect
+    output=$(iCmd "pm install ${cmd} \"/data/local/tmp/${outputFileName}\"" 2>&1); echo "$output"
+    [ $DisablePlayProtect -eq 1 ] && iCmd "settings put global package_verifier_user_consent 1"  # Enabled Play Protect
     if [ $DisableVerifyAdbInstalls -eq 1 ]; then
-      [ $Android -le 10 ] && ~/rish -c "settings put global package_verifier_enable 1" || ~/rish -c "settings put global verifier_verify_adb_installs 1"  # Enabled Verify Adb Installs
-    fi
-    if [[ $output == *"Downgrade detected"* ]] && [ $KeepsData -eq 1 ]; then
-      echo -e "${Green}$appName uninstall successfully with keeps app data.${Reset}\n${Yellow}Don't forget to restart Shizuku & Simplify after reboot!${Reset}"
-      ~/rish -c "cmd package uninstall -k $pkgName"
-      cp "$outputAPK" "$POST_INSTALL"
-      sleep 12
-      ~/rish -c "reboot"
-    fi
-    am start -n "$activityClass" &> /dev/null  # launch app after update
-    if [ $? != 0 ]; then
-      ~/rish -c "monkey -p $pkgName -c android.intent.category.LAUNCHER 1" > /dev/null 2>&1
-    fi
-    $HOME/rish -c "rm -f '/data/local/tmp/$outputFileName'"  # Cleanup tmp APK
-  elif "$HOME/adb" -s $(~/adb devices | grep "device$" | awk '{print $1}' | tail -1) shell "id" >/dev/null 2>&1; then
-    [ $DisablePlayProtect -eq 1 ] && "$HOME/adb" -s $(~/adb devices | grep "device$" | awk '{print $1}' | tail -1) shell "settings put global package_verifier_user_consent -1"  # Disabled Play Protect
-    if [ $DisableVerifyAdbInstalls -eq 1 ]; then
-      [ $Android -le 10 ] && ~/adb -s $("$HOME/adb" devices 2>/dev/null | grep "device$" | awk '{print $1}' | tail -1) shell "settings put global package_verifier_enable 0" || ~/adb -s $("$HOME/adb" devices 2>/dev/null | grep "device$" | awk '{print $1}' | tail -1) shell "settings put global verifier_verify_adb_installs 0"  # Disable Verify Adb Installs
-    fi
-    output=$(~/adb -s $(~/adb devices 2>/dev/null | grep "device$" | awk '{print $1}' | tail -1) shell pm install ${cmd} \"$outputAPK\" 2>&1); echo "$output"
-    #$HOME/adb -s $(~/adb devices 2>/dev/null | grep "device$" | awk '{print $1}' | tail -1) install ${cmd} "$outputAPK" 2>&1
-    #~/adb -s $(~/adb devices 2>/dev/null | grep "device$" | awk '{print $1}' | tail -1) shell cmd package install ${cmd} "$outputAPK" > /dev/null 2>&1
-    [ $DisablePlayProtect -eq 1 ] && ~/adb -s $(~/adb devices 2>/dev/null | grep "device$" | awk '{print $1}' | tail -1) shell "settings put global package_verifier_user_consent 1"  # Enabled Play Protect
-    if [ $DisableVerifyAdbInstalls -eq 1 ]; then
-      [ $Android -le 10 ] && ~/adb -s $(~/adb devices 2>/dev/null | grep "device$" | awk '{print $1}' | tail -1) shell "settings put global package_verifier_enable 1" || ~/adb -s $(~/adb devices 2>/dev/null | grep "device$" | awk '{print $1}' | tail -1) shell "settings put global verifier_verify_adb_installs 1"  # Enabled Verify Adb Installs
+      [ $Android -le 10 ] && iCmd "settings put global package_verifier_enable 1" || iCmd "settings put global verifier_verify_adb_installs 1"  # Enabled Verify Adb Installs
     fi
     if [[ $output == *"Downgrade detected"* ]] && [ $KeepsData -eq 1 ]; then
       echo -e "${Green}$appName uninstall successfully with keeps app data.${Reset}\n${Yellow}Don't forget to restart Simplify after reboot!${Reset}"
-      ~/adb -s $(~/adb devices 2>/dev/null | grep "device$" | awk '{print $1}' | tail -1) shell "cmd package uninstall -k $pkgName"
+      iCmd "cmd package uninstall -k $pkgName"
       cp "$outputAPK" "$POST_INSTALL"
       sleep 12
-      "$HOME/adb" -s $(~/adb devices 2>/dev/null | grep "device$" | awk '{print $1}' | tail -1) "reboot"
+      iCmd "reboot"
     fi
     am start -n "$activityClass" &> /dev/null  # launch app after update
-    [ $? != 0 ] && ~/adb -s $(~/adb devices 2>/dev/null | grep "device$" | awk '{print $1}' | tail -1) shell "monkey -p $pkgName -c android.intent.category.LAUNCHER 1" > /dev/null 2>&1
-  elif [ "$Android" -le "6" ]; then
-    am start -a android.intent.action.VIEW -t application/vnd.android.package-archive -d "file://$outputAPK" > /dev/null 2>&1  # Activity Manager
-    sleep 15 && am start -n "$activityClass" &> /dev/null  # launch app after update
-  else
-    termux-open --view "$outputAPK"  # install apk using Session installer
-    sleep 15 && am start -n "$activityClass" &> /dev/null  # launch app after update
-  fi
-  
-  if [ $su -eq 1 ] || "$HOME/rish" -c "id" >/dev/null 2>&1 || "$HOME/adb" -s $(~/adb devices 2>/dev/null | grep "device$" | awk '{print $1}' | tail -1) shell "id" >/dev/null 2>&1; then
+    if [ $? != 0 ]; then
+      iCmd "monkey -p $pkgName -c android.intent.category.LAUNCHER 1" > /dev/null 2>&1
+    fi
+    iCmd "rm -f '/data/local/tmp/$outputFileName'"
     if [ $EnableRoolback -eq 1 ]; then
       read -r -p "Is the $appName app working correctly? [Y/n]: " response
       if [[ "$response" == [Yy]* ]]; then
         echo "Great! The $appName app is working properly."
       else
         echo -e "$running Roolback to previous version.."
-        if [ $su -eq 1 ]; then
-          if [ "$(su -c 'getenforce 2>/dev/null')" = "Enforcing" ]; then
-            su -c "setenforce 0"
-            su -c "pm rollback-app $pkgName"
-            su -c "setenforce 1"
-          else
-            su -c "pm rollback-app $pkgName"
-          fi
-        elif "$HOME/rish" -c "id" >/dev/null 2>&1; then
-          $HOME/rish -c "pm rollback-app $pkgName"
-        elif "$HOME/adb" -s $(~/adb devices 2>/dev/null | grep "device$" | awk '{print $1}' | tail -1) shell "id" >/dev/null 2>&1; then
-          $HOME/adb -s $(~/adb devices 2>/dev/null | head -2 | tail -1 | awk '{print $1}') shell "pm rollback-app $pkgName"
-        fi
+        iCmd "pm rollback-app $pkgName"
       fi
     fi
+  else
+    if [ $Android -le 6 ]; then
+      am start -a android.intent.action.VIEW -t application/vnd.android.package-archive -d "file://$outputAPK" > /dev/null 2>&1  # Activity Manager
+      sleep 15 && am start -n "$activityClass" &> /dev/null  # launch app after update
+    else
+      termux-open --view "$outputAPK"  # install apk using Session installer
+      sleep 15 && am start -n "$activityClass" &> /dev/null  # launch app after update
+    fi
+  fi
+  if [ $su -eq 1 ]; then
+    [ $writeSELinux -eq 1 ] && su -c "setenforce 1"
   fi
   [ $RmFileAfterInstallation -eq 1 ] && rm -f "$outputAPK"
 }
