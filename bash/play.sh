5a9962b74e9243aa9096166a06c4ab95ee388f76
Arghyadeep <139842491+arghya339@users.noreply.github.com>
2025-11-28 19:34:43 +0530

    fix

--- bash/play.sh
+++ bash/play.sh
@@ -115,8 +115,9 @@ gPlayApiSearchApps() {
     searchAppsUrl=("$searchUrl?q=${query}&c=3&ksm=1")
     while true; do
       curl -sL "${searchAppsUrl[$((page-1))]}" "${Headers[@]}" -H "Accept: application/x-protobuf" -o "search.protobuf"
-      search=$(protoc --decode_raw < search.protobuf) && rm -f search.protobuf
+      search=$(protoc --decode_raw < search.protobuf)
       detailsList "$search"
+      rm -f search.protobuf
       declare -a appInfo offerTypesS containsAdsS
       for ((i=0; i<${#pkgs[@]}; i++)); do
         [ ${offerTypes[i]} -eq 1 ] && offerTypesS+=(Free) || offerTypesS+=(Paid)
