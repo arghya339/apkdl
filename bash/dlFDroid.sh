f5159aadbd7ef3e3b9eb6b0a727b854a43a73bc5
Arghyadeep <139842491+arghya339@users.noreply.github.com>
2025-11-30 18:24:24 +0530

    fix

--- bash/dlFDroid.sh
+++ bash/dlFDroid.sh
@@ -58,13 +58,17 @@ dlFDroid() {
 FDroidVersionsList() {
   #responseJson=$(curl -sL https://f-droid.org/api/v1/packages/$pkg)
   responseJson=$(curl -sL "https://f-droid.org/en/packages/$pkg/" | pup 'ul.package-versions-list li.package-version json{}')  # | jq . > response.json
+  #responseYml=$(curl -sL "https://gitlab.com/fdroid/fdroiddata/-/raw/master/metadata/$pkg.yml")  # pkg install yq | brew install yq
   versionsCount=$(jq 'length' <<< "$responseJson")
   if [ $versionsCount -ge 1 ]; then
     mapfile -t versionNames < <(jq -r '.[] | if .id == "latest" then .children[1].children[0].name else .children[0].children[0].name end' <<< "$responseJson")
+    #mapfile -t versionNames < <(yq eval '.Builds[].versionName' - <<< "$responseYml" | tail -r)
     mapfile -t versionCodes < <(jq -r '.[] | if .id == "latest" then .children[1].children[1].name else .children[0].children[1].name end' <<< "$responseJson")
+    #mapfile -t versionCodes < <(yq eval '.Builds[].versionCode' - <<< "$responseYml" | tail -r)
     suggestedVersionCode=$(jq -r '.[] | select(.id == "latest") | .children[1].children[1].name' <<< "$responseJson")
     mapfile -t addedDates < <(jq -r '.[] | .children[]? | select(.class? == "package-version-header") | .text' <<< "$responseJson" | grep -o 'Added on [A-Za-z 0-9,]*')
     mapfile -t pkgNativeCodes < <(jq -r '.[] | .children[]? | select(.class? == "package-version-nativecode") | .children[]?.text?' <<< "$responseJson")
+    #mapfile -t pkgNativeCodes < <(yq eval '.Builds[] | .gradleprops[] | select(contains("ABI_FILTERS")) | sub("ABI_FILTERS=", "")' - <<< "$responseYml" | tail -r)
     mapfile -t pkgVersionRequirement < <(jq -r '.[] | .children[]? | select(.class? == "package-version-requirement") | .text?' <<< "$responseJson" | sed 's/This version requires //' | sed 's/\.$//')
     permissionLabel=$(jq -r --arg vCode "$suggestedVersionCode" '.[] | select((.children[]? | select(.class? == "package-version-header") | .children[]?.name? // empty) == $vCode) | .. | objects | select(.class? == "permission-label") | .text' <<< "$responseJson")
     permissionLabelWithDescription=$(jq -r --arg vCode "$suggestedVersionCode" '.[] | select((.children[]? | select(.class? == "package-version-header") | .children[]?.name? // empty) == $vCode) | .children[]?.children[]?.children[]? | select(.class? == "package-version-permissions-list") | .children[] | {label: (.children[]? | select(.class? == "permission-label") | .text), desc: (.children[]? | select(.class? == "permission-description") | .text)} | "\(.label): \(.desc)"' <<< "$responseJson")
