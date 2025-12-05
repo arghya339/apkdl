a001b15470db0acb076b205ea93ba99c1e55f858
Arghyadeep <139842491+arghya339@users.noreply.github.com>
2025-12-05 14:28:52 +0530

    fix

--- bash/otherSources.sh
+++ bash/otherSources.sh
@@ -883,14 +883,20 @@ while true; do
       [ $? -ne 0 ] && continue
       dlOther
       if [ $? -eq 0 ]; then
-        if { [ $isMacOS -eq 1 ] && [ -n "$serial" ]; } || [ $isAndroid -eq 1 ]; then
+        ext="${fileName##*.}"
+        if { [[ $isMacOS -eq 1 && ( ( "$ext" == "apk" && -n "$serial" ) || "$ext" == "dmg" || "$ext" == "pkg" ) ]]; } || [[ $isAndroid -eq 1 ]]; then
           buttons=("<Yes>" "<No>"); confirmPrompt "Do you want to install $fileName" "buttons" && opt="Yes" || opt="No"
           if [ "$opt" == "Yes" ]; then
-            [ $isMacOS -eq 1 ] && adbInstall "$filePath"
-            [ $isAndroid -eq 1 ] && apkInstall "$filePath"
+            if [ $isAndroid -eq 1 ]; then
+              apkInstall "$filePath"
+            elif [ $isMacOS -eq 1 ]; then
+              ([ "$ext" == "apk" ] && [ -n "$serial" ]) && adbInstall "$filePath"
+              ([ "$ext" == "dmg" ] || [ "$ext" == "pkg" ]) && open "$filePath"
+            fi
           fi
         fi
         echo; read -p "Press Enter to continue..."
+        { [[ $isMacOS -eq 1 && ( "$ext" == "dmg" || "$ext" == "pkg" ) && $RmFileAfterInstallation -eq 1 ]]; } && rm -f "$filePath"
       fi
       ;;
     APKCombo)
